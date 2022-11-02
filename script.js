442d38d8e6869056ae276a6cc5b94b5760c5432f
youarenomatchforme <ghadoorah07@gmail.com>
2022-11-02 19:02:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop();
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99);
 // طبّق الخطوة 6 أسفل  هذا الكومنت
-
-
+array.forEach((grades) => {
+  all_grades_div.innerHTML += `<div>${grades}</div>`;
+});
 
 function search() {
-  
   // طبّق الخطوة 7و8 أسفل هذا الكومنت
-  
-  
+  let randomIndex = Math.floor(Math.random() * grades.length);
   // طبّق الخطوة 9 أسفل هذا الكومنت
-
+  random_grade_div.innerHTML = `<div>${grades[randomIndex]}</div>`;
 }
-
-
-
